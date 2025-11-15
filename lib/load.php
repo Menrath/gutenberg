ff278b2ffd05049cdcd527e05a5fe881e81b242e
Aki Hamano <tetsuaki.hamano@gmail.com>
2025-11-15 12:33:08 +0900

    Make feature experimental

--- lib/load.php
+++ lib/load.php
@@ -62,7 +62,6 @@ if ( class_exists( 'WP_REST_Controller' ) ) {
 	// Plugin specific code.
 	require_once __DIR__ . '/class-wp-rest-global-styles-controller-gutenberg.php';
 	require_once __DIR__ . '/class-wp-rest-edit-site-export-controller-gutenberg.php';
-	require_once __DIR__ . '/class-wp-rest-icon-controller-gutenberg.php';
 	require_once __DIR__ . '/rest-api.php';
 
 	require_once __DIR__ . '/experimental/rest-api.php';
@@ -142,7 +141,6 @@ require __DIR__ . '/class-wp-theme-json-gutenberg.php';
 require __DIR__ . '/class-wp-theme-json-resolver-gutenberg.php';
 require __DIR__ . '/class-wp-theme-json-schema-gutenberg.php';
 require __DIR__ . '/class-wp-duotone-gutenberg.php';
-require __DIR__ . '/class-wp-icons-registry-gutenberg.php';
 require __DIR__ . '/blocks.php';
 require __DIR__ . '/block-editor-settings.php';
 require __DIR__ . '/client-assets.php';
@@ -188,4 +186,9 @@ if ( gutenberg_is_experiment_enabled( 'gutenberg-media-processing' ) ) {
 if ( gutenberg_is_experiment_enabled( 'gutenberg-full-page-client-side-navigation' ) ) {
 	require __DIR__ . '/experimental/interactivity-api/class-gutenberg-interactivity-api-full-page-navigation.php';
 	Gutenberg_Interactivity_API_Full_Page_Navigation::instance();
+}
+
+if ( gutenberg_is_experiment_enabled( 'gutenberg-svg-icon-registry' ) ) {
+	require __DIR__ . '/experimental/class-wp-icons-registry.php';
+	require __DIR__ . '/experimental/class-wp-rest-icon-controller.php';
 }
