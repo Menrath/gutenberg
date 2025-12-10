000dbb2879cbc4bb879ec05edc3e330e4e377b61
Dave Smith <getdavemail@gmail.com>
2025-12-10 13:02:18 +0000

    Add URL validation

--- packages/block-editor/src/components/link-control/search-input.js
+++ packages/block-editor/src/components/link-control/search-input.js
@@ -3,6 +3,7 @@
  */
 import { forwardRef, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import deprecated from '@wordpress/deprecated';
 
 /**
  * Internal dependencies
@@ -11,7 +12,6 @@ import { URLInput } from '../';
 import LinkControlSearchResults from './search-results';
 import { CREATE_TYPE } from './constants';
 import useSearchHandler from './use-search-handler';
-import deprecated from '@wordpress/deprecated';
 
 // Must be a function as otherwise URLInput will default
 // to the fetchLinkSuggestions passed in block editor settings
@@ -143,6 +143,11 @@ const LinkControlSearchInput = forwardRef(
 						showInitialSuggestions
 					}
 					customValidity={ customValidityProp }
+					// Suppress the "(Required)" indicator that appears when validation
+					// is triggered. The field is still required for validation purposes,
+					// but we don't want to show the indicator as it looks cluttered
+					// in the link control UI.
+					markWhenOptional
 					onSubmit={
 						onSubmitProp ||
 						( ( suggestion, event ) => {
